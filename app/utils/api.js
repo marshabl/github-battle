853601bbbc35e1670555afb03feb028de8239892
Blair Marshall <marshabl12@gmail.com>
2019-02-22 14:52:53 -0800

    Add firebase for production

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,23 +1,24 @@
 var axios = require('axios');
-var id = "YOUR_CLIENT_ID";
-var sec = "YOUR_SECRET_ID";
+
+var id = "78696aea67aead5d4540";
+var sec = "c5716f6757fa2b57427c930acd36917f94823de4";
 var params = "?client_id=" + id + "&client_secret=" + sec;
 
 function getProfile (username) {
-  return axios.get('https://api.github.com/users/' + username)
-    .then(function () {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function (user) {
       return user.data;
-    })
+    });
 }
 
 function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + '&per_page=100')
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
 }
 
 function getStarCount (repos) {
   return repos.data.reduce(function (count, repo) {
-    return count + repo.stargazers_count;
-  }, 0)
+    return count + repo.stargazers_count
+  }, 0);
 }
 
 function calculateScore (profile, repos) {
@@ -44,22 +45,21 @@ function getUserData (player) {
       profile: profile,
       score: calculateScore(profile, repos)
     }
-  })
+  });
 }
 
 function sortPlayers (players) {
   return players.sort(function (a,b) {
     return b.score - a.score;
-  })
+  });
 }
 
 module.exports = {
   battle: function (players) {
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
-      .catch(handleError)
+      .catch(handleError);
   },
-
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
